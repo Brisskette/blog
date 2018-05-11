da1494f94f74a57a3809257848c705ab5085e3e1
Antoine Brissy <antoinebrissy@gmail.com>
2018-05-11 11:34:13 +0200

    Prepare for Heroku

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class ArticlesTest < ApplicationSystemTestCase
   # test "visiting the index" do
-  #   visit articles_url
+  #   visit root_url
   #
   #   assert_selector "h1", text: "Article"
   # end
